4fb49ddacebc173a245afce91bb8b95153963bc5
zhenyangze <zhenyangze@xiaoyezi.com>
2022-04-14 19:19:51 +0800

    master: [feature] check template

--- routes/web.go
+++ routes/web.go
@@ -5,14 +5,18 @@ import (
 	"gitee.com/zhenyangze/gin-framework/app/modules/index"
 	"gitee.com/zhenyangze/gin-framework/app/modules/users"
 	websocket "gitee.com/zhenyangze/gin-framework/app/modules/websockets"
+	"gitee.com/zhenyangze/gin-framework/helpers"
 	"github.com/gin-gonic/gin"
 	_ "go.uber.org/automaxprocs"
 )
 
 func Web(router *gin.Engine) {
 	router.Use(middleware.DefaultRequestID())
+
 	// 加载模板文件
-	router.LoadHTMLGlob("resources/views/**/*")
+	if helpers.IsDir("resources/views/") {
+		router.LoadHTMLGlob("resources/views/**/*")
+	}
 
 	//service router
 	index.Router(router)
